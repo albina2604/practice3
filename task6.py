71992b2dc940b960bf795a9a3d47ad28a40e67fa
albina2604 <albinamilshyna2604@gmail.com>
2022-10-14 21:21:52 +0300

    Add task6.py

---
+++ task6.py
@@ -0,0 +1,14 @@
+import math
+from os import remove
+
+n = int(input())
+pupils = []
+mark = 3
+for _ in range(n):
+    pupil = [str(s) for s in input().split()]
+    pupil[1:] = [int(i) for i in pupil[1:]]
+    pupil.append(sum(pupil[1:])/mark)
+    pupils.insert(0, pupil)
+
+pupils_sorted = sorted(pupils)
+print(pupils_sorted)
