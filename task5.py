a812801ebcff8826bff1ea8487e8a3234da11b69
albina2604 <albinamilshyna2604@gmail.com>
2022-10-15 14:07:32 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,11 +1,14 @@
 n = int(input())
 pupils = dict()
 mark = 3
-marks = [0 for i in range(mark)]
+marks = [0] * mark
 for _ in range(n):
-    pupil = [str(s) for s in input().split()]
+    pupil = input().split()
+    last_name = pupil[0]
     pupils[pupil[0]] = [int(i) for i in pupil[1:]]
     for i in range(mark):
-        marks[i] += pupils[pupil[0]][i]
-for i in range(mark):
-    print(f'{marks[i]/n:.2f}')
+        marks[i] += pupils[last_name][i]
+
+print('-' * 20)
+avg_marks = (str('%.2f' % (marks[i]/n)) for i in range(mark))
+print(' '.join(avg_marks))
